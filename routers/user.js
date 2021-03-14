b54124cb25d5d6a07c805bdc0b4e85db257cb1b9
sharathankush <sharathankush@gmail.com>
2021-03-14 19:44:27 +0530

    added User Upade feature

--- routers/user.js
+++ routers/user.js
@@ -1,9 +1,10 @@
 const express = require("express")
 const router  = express.Router()
-const {getUserById ,getUser} = require("../controllers/user")
+const {getUserById ,getUser,updateUser} = require("../controllers/user")
 const {isSignedIn, isAuthenticate,isAdmin} = require("../controllers/auth")
 
 router.param("userId", getUserById);
 router.get("/user/:userId", isSignedIn,isAuthenticate,getUser);
+router.put("/user/:userId", isSignedIn,isAuthenticate,updateUser);
 
 module.exports = router ;
