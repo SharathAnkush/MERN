f4ae79f757c5a5700f46630437296e1ece0e1314
sharathankush <sharathankush@gmail.com>
2021-03-19 08:46:52 +0530

    get Producet

--- routers/product.js
+++ routers/product.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const router = express.Router()
 
-const {getProductById, createProduct} = require("../controllers/product")
+const {getProductById, createProduct,getProduct,photo} = require("../controllers/product")
 const {isSignedIn,isAuthenticate,isAdmin} = require("../controllers/auth")
 const {getUserById} = require("../controllers/user")
 
@@ -10,5 +10,7 @@ router.param("userId",getUserById)
 router.param("productId",getProductById)
 
 router.post("/product/create/:userId", isSignedIn, isAuthenticate, isAdmin, createProduct )
+router.get("/product/:producetId",getProduct);
+router.get("/product/photo/:producetId",photo);
 
 module.exports = router;
