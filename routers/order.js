69ce0f1bf4bda5e3e3e0052dc60ed683ffdf18b4
sharathankush <sharathankush@gmail.com>
2021-03-20 11:30:10 +0530

    get Order Status and Update Status

--- routers/order.js
+++ routers/order.js
@@ -4,7 +4,7 @@ const router = express.Router()
 const {isSignedIn,isAuthenticate,isAdmin} = require("../controllers/auth")
 const {getUserById,pushOderInPurchaseList} = require("../controllers/user")
 const {updateStock} = require("../controllers/product")
-const {getOrderById,createOrder} = require("../controllers/order")
+const {getOrderById,createOrder,getAllOrders,getOrderStatus,updateStatus} = require("../controllers/order")
 
 
 router.param("userId", getUserById)
@@ -19,5 +19,18 @@ router.post(
     updateStock,
     createOrder 
 );
+
+//Read
+router.get(
+    "/order/all/:userId",
+    isSignedIn,
+    isAuthenticate,
+    isAdmin,
+    getAllOrders
+);
+
+//Status Order
+router.get("/order/status/:userId",isSignedIn,isAuthenticate,isAdmin,getOrderStatus)
+router.put("/order/:orerId/status/:userId",isSignedIn,isAuthenticate,isAdmin,updateStatus)
 
 module.exports = router;
